9f1ef001c23e9a7502363624fc0bcd0558a2a719
Rudro <bisalsaha42@gmail.com>
2025-09-18 03:45:53 +0600

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Root from './assets/Root/Root.jsx'
 const router=createBrowserRouter([
